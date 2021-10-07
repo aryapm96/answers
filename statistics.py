8ee60d61fded1f3a0b3432a0eb4c87bcb929253c
Arya P M <81687060+aryapm96@users.noreply.github.com>
2021-10-07 14:32:24 +0530

    answer for question no:3

---
+++ statistics.py
@@ -0,0 +1,11 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", "-f", type=str, required=True)
+parser.add_argument('--country', type=str, required=True, help='Country to perform operation for')
+parser.add_argument('--o{avg,min,max}', type=int,help='Operation to perform on dates.(Default avg)')
+parser.add_argument('--from', type=int,help='Starting year (inclusive)')
+parser.add_argument('--to', type=int,help='Ending year (inclusive)')
+args = parser.parse_args()
+
+if args.country:
+    print(args.country,args.min,args.max,args.avg,args.from,args.to)
